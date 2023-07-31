fbf7bd48b2221e515bde4713efac2b8ea648e6e3
mvm283 <mvm283@gmail.com>
2023-08-01 00:42:03 +0200

    [DB_PRJ] - unit test has been created

---
+++ src/test/java/com/db/second/signals/impl/SignalAction1Test.java
@@ -0,0 +1,37 @@
+package com.db.second.signals.impl;
+
+import com.db.second.algo.Algo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.mockito.Mockito.doThrow;
+
+@SpringBootTest
+class SignalAction1Test {
+
+    @Mock
+    private Algo algo;
+    @InjectMocks
+    private SignalAction1 signalAction;
+
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+
+    @Test
+    public void handleSignalTest(){
+        Assertions.assertTrue(signalAction.handleSignal());
+    }
+    @Test
+    public void handleSignalTestUnSuccessfull() {
+
+        doThrow(new RuntimeException()).when(algo).setUp();
+        Assertions.assertFalse(signalAction.handleSignal());
+    }
+}
