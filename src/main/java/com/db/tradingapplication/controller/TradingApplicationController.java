6aa795bc2a933fab6398db9d57dec13690272cd5
mvm283 <mvm283@gmail.com>
2023-08-01 17:31:39 +0200

    [DB_PRJ] - Logger has been added

--- src/main/java/com/db/tradingapplication/controller/TradingApplicationController.java
+++ src/main/java/com/db/tradingapplication/controller/TradingApplicationController.java
@@ -2,6 +2,8 @@ package com.db.tradingapplication.controller;
 
 import com.db.tradingapplication.service.ImplementationFactory;
 import com.db.tradingapplication.signals.Signals;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController()
 @RequestMapping(value ="api/v1/")
 public class TradingApplicationController {
+    Logger logger = LoggerFactory.getLogger(TradingApplicationController.class);
     private final ImplementationFactory implementationFactory;
 
     public TradingApplicationController(ImplementationFactory implementationFactory) {
@@ -21,11 +24,13 @@ public class TradingApplicationController {
         Signals implementation = implementationFactory.createInstance(signal);
 
         if(implementation==null){
+            logger.error("Signal " + signal + " has not been implemented");
             return new ResponseEntity<>("Signal " + signal + " has not been implemented", HttpStatus.BAD_REQUEST);
         }
 
         implementation.handleSignal();
 
+        logger.info("Signal " + signal + " processed successfully");
         return new ResponseEntity<>("Signal " + signal + " processed successfully", HttpStatus.OK);
     }
 }
--- src/main/java/com/db/tradingapplication/service/impl/ImplementationFactoryImpl.java
+++ src/main/java/com/db/tradingapplication/service/impl/ImplementationFactoryImpl.java
@@ -3,12 +3,16 @@ package com.db.tradingapplication.service.impl;
 import com.db.tradingapplication.service.ImplementationFactory;
 import com.db.tradingapplication.signals.Signals;
 import com.db.tradingapplication.signals.impl.SignalAction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ImplementationFactoryImpl implements ImplementationFactory {
+    Logger logger = LoggerFactory.getLogger(ImplementationFactoryImpl.class);
     private final ApplicationContext context;
 
     @Autowired
@@ -20,10 +24,13 @@ public class ImplementationFactoryImpl implements ImplementationFactory {
     public Signals createInstance(int signalNumber) {
         String className= SignalAction.class.getSimpleName()+signalNumber;
         try {
-            return context.getBean(className, Signals.class);
+            logger.info("getting signal processor");
+            Signals bean = context.getBean(className, Signals.class);
+            logger.info("getting signal processor has done");
+            return bean;
         }
-        catch (Exception e) {
-            throw new RuntimeException("There is no implementation for the Signal number : " +signalNumber);
+        catch (NoSuchBeanDefinitionException e) {
+            throw new NoSuchBeanDefinitionException("There is no implementation for the Signal number : " +signalNumber);
         }
     }
 }
